0892420bbb00f68c9706092844579f3530affadc
vjcitn <stvjc@channing.harvard.edu>
2023-11-28 14:25:25 -0500

    make eval optional in train_eval

--- R/train_eval.R
+++ R/train_eval.R
@@ -1,8 +1,9 @@
 
-#' run training and evaluation
+#' run training and (optionally) evaluation
 #' @param tripout list resulting from `triples_to_hdf5`
 #' @param pbg torchbiggraph reference
 #' @param evind numeric(1) index in edge_paths to use for evaluation
+#' @param do_eval logical(1) if TRUE an evaluation is performed
 #' @note when a full set of train/validate/test paths are provided,
 #' then evind should be set to 3
 #' @examples
@@ -11,9 +12,9 @@
 #' tt2 = list()
 #' tt2$config = pyatt$evolve(tt$config, num_epochs=4L, num_uniform_negs=1000L,
 #'    num_gpus=1L)
-#' train_eval(tt2, pbg)
+#' train_eval(tt2, pbg, do_eval=TRUE)
 #' @export
-train_eval = function(tripout, pbg, evind=3) {
+train_eval = function(tripout, pbg, evind=3, do_eval=FALSE) {
   ut = pbg$util
   ut$setup_logging()
   si = ut$SubprocessInitializer()
@@ -34,9 +35,11 @@ train_eval = function(tripout, pbg, evind=3) {
 
   pbg$train$train(trc, subprocess_init=si)
   
-  nr = lapply( trc$relations, function(x) pyatt$evolve(x, all_negs=TRUE))
-  trc2 = pyatt$evolve(trc, edge_paths = list(tripout$config$edge_paths[[evind]]),
+  if (do_eval) {
+    nr = lapply( trc$relations, function(x) pyatt$evolve(x, all_negs=TRUE))
+    trc2 = pyatt$evolve(trc, edge_paths = list(tripout$config$edge_paths[[evind]]),
       relations=nr, num_uniform_negs = 0L)
-  pbg$eval$do_eval(trc2, subprocess_init = si)
+    pbg$eval$do_eval(trc2, subprocess_init = si)
+    }
 }
 
