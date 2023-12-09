b6651e3ce2155d030728a0e7939f6daea8a4d150
vjcitn <stvjc@channing.harvard.edu>
2023-12-09 13:53:56 -0500

    almost replicating gini vs max plot

--- R/gini_from_edgeR.R
+++ R/gini_from_edgeR.R
@@ -20,5 +20,34 @@
 #' @export
 gini = function(x) .gini_from_edgeR(x)
 
+#' obtain the simba norm metric for genes
+#' @param sce SingleCellExperiment with simba scores available
+#' @export
+norm_scores = function(sce) {
+ stopifnot("sprobs" %in% altExpNames(sce))
+ x = qlogis(assay(altExps(sce)$sprobs)) # undo the softmax
+ t(apply(x,1,function(x)(x-log(mean(exp(x))))))
+}
+
 #' obtain the simba max metric for genes
 #' @param sce SingleCellExperiment with simba scores available
+#' @param N number of cells with highest scores to average
+#' @examples
+#' data(nn50)
+#' data(t3k)
+#' t3k = add_simba_scores(nn50, t3k)
+#' gi = gini(t(assay(altExps(t3k)$sprobs)))
+#' ma = as.numeric(max_scores(t3k))
+#' plot(ma, gi)
+#' mydf = data.frame(max=as.numeric(ma), gini=gi, gene=rowData(t3k)$Symbol)
+#' litd = mydf[order(mydf$gini,decreasing=TRUE),][1:25,]
+#' hh = mydf[mydf$gene=="GAPDH",] 
+#' litd = rbind(litd, hh)
+#' mm = ggplot(mydf, aes(x=max, y=gini, text=gene)) + geom_point() 
+#' mm + ggrepel::geom_text_repel(data=litd, 
+#'          aes(x=max, y=gini, label=gene), colour="purple")
+#' @export
+max_scores = function(sce, N=50) {
+ nn = norm_scores(sce)
+ t(apply(nn,1,function(x)mean(sort(x,decreasing=TRUE)[seq_len(N)])))
+}
