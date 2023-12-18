006f33b9d3452a2b3f5b2e1bfe42d4d7d113ec3c
vjcitn <stvjc@channing.harvard.edu>
2023-12-18 18:32:45 +0000

    new 50 epoch object with training stats

---
+++ inst/scripts/newemb50.R
@@ -0,0 +1,22 @@
+
+
+
+# this script works from make_triples output for discretized pbmc3k
+# used to revise nn50.rda on 2 Dec 2023 to deal with duplicated rownames
+
+Sys.setenv("RETICULATE_PYTHON"="/usr/bin/python3.10")
+
+library(BiocPBG)
+library(SingleCellExperiment)
+data(t3k)
+rownames(t3k) = make.names(rownames(t3k), unique=TRUE)
+print(length(unique(rownames(t3k))))
+#
+pbg = import("torchbiggraph")
+palib = import("pathlib")
+   
+
+
+nn50c = sce_to_embeddings(t3k, "myfold50b", N_EPOCHS=50L, BATCH_SIZE=1000L,
+    N_GENES=5000L, N_GPUS=0L, pbg=pbg, palib=palib)
+save(nn50c, file="nn50c.rda")
